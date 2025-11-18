f621eb66aa9e80b198cc9f8ccc2a06fafecab76d
vivithequeen <vivithequeen1@gmail.com>
2025-11-18 18:46:53 -0400

    ok it should be working now

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
+    root: '', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
