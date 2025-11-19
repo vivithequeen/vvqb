fd6a741ca7e942479d5bfb6220f5d565c512751e
vivithequeen <vivithequeen1@gmail.com>
2025-11-18 21:31:47 -0400

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/src/Three.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.166.1/build/three.module.js';
 
 const width = window.innerWidth, height = window.innerHeight;
 
