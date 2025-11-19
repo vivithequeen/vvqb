cf8293bfb32950d6a60a898529058b7847133190
vivithequeen <vivithequeen1@gmail.com>
2025-11-18 21:12:48 -0400

    im going to go study chem if this doesnt work

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
+    base: process.env.NODE_ENV === 'production' ? '/vvqb/' : '',
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
